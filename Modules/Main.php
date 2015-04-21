6c7986b9e59bd42979cbfca3f08caf9680a8b4d3
caranille <mycaranille@gmail.com>
2015-04-21 20:42:04 +0200

    Fix

--- Modules/Main.php
+++ Modules/Main.php
@@ -2,7 +2,6 @@
 	error_reporting(E_ALL); 
 	$timestart = microtime(true);
 	session_start();
-
 	require_once $_SESSION['File_Root']. '/Kernel/Include.php';
 	require_once $_SESSION['File_Root']. '/HTML/Header.php';
 
--- index.php
+++ index.php
@@ -1,21 +1,26 @@
 <?php
 session_start();
+
 $Config = 'Kernel/Config/SQL.php';
 $Size = filesize($Config); 
+
 if ($Size == 0) 
 {
 	header('Location: install.php');
 }
 else
 {
-	//IF THE $_SESSION['File_Root'] AS EMPTY WE CREATE THE SESSION WITH THE ABSOLUT PATH
-	if (empty($_SESSION['File_Root']) && empty($_SESSION['Link_Root']))
+	require_once("Kernel/Config/Server.php");
+
+	$_SESSION['File_Root'] = $File_Root;
+	$_SESSION['Link_Root'] = $Link_Root;
+
+	$Link = 'http://' .$_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
+
+	if ($Link != $_SESSION['Link_Root'])
 	{
-		require_once("Kernel/Config/Server.php");
-		$_SESSION['File_Root'] = $File_Root;
-		$_SESSION['Link_Root'] = $Link_Root;
-		require_once $_SESSION['File_Root']. '/Kernel/Include.php';
-		header('Location: Modules/index.php');
+		$Link_Root = $_SESSION['Link_Root'];
+		header("Location: $Link_Root/index.php");
 	}
 	else
 	{
