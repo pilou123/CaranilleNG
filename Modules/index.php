ee3b2ea1f364340f9c909ad453b082d2f7ed1881
Caranille <mycaranille@gmail.com>
2015-04-26 17:07:43 +0200

    Update index.php

--- Modules/index.php
+++ Modules/index.php
@@ -6,7 +6,27 @@ session_start();
 require_once $_SESSION['File_Root']. '/Kernel/Include.php';
 require_once $_SESSION['File_Root']. '/HTML/Header.php';
 
-SQL_News_List();
+//Search and display game news
+$Resultat = $bdd->query("SELECT * FROM Caranille_News ORDER BY News_ID desc");
+while ($News = $Resultat->fetch())
+{
+  echo '<table>';
+   echo '<tr>';
+  echo '<th>';
+  echo "$Main_0 " .$News['News_Date']. " $Main_1 " .$News['News_Account_Pseudo']. "";
+  echo '</th>';
+  echo '</tr>';
+  
+  echo '<tr>';
+  echo '<td>';
+  echo '<h4>' .$News['News_Title']. '</h4>';
+  echo '' .stripslashes(nl2br($News['News_Message'])). '';
+  echo '</td>';
+  
+  echo '</tr>';
+  echo '</table><br /><br />';
+}
+$Resultat->closeCursor();
 
 require_once $_SESSION['File_Root'] .'/HTML/Footer.php';
 ?>
