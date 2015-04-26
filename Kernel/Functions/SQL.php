e9f58750af8569938a501f3702c2b829a0ec28ad
Caranille <mycaranille@gmail.com>
2015-04-26 17:02:34 +0200

    Update SQL.php

--- Kernel/Functions/SQL.php
+++ Kernel/Functions/SQL.php
@@ -8,32 +8,4 @@
     {
     	echo 'An error as occured, Cannot connect to the database. Error: ' . $e->getMessage();
     }
-// END OF LAUNCH THE CONNECTION
-
-function SQL_News_List()
-{
-    global $bdd;
-    global $Main_0;
-    global $Main_1;
-        
-	$Resultat = $bdd->query("SELECT * FROM Caranille_News ORDER BY News_ID desc");
-	while ($News = $Resultat->fetch())
-	{
-		echo '<table>';
-			echo '<tr>';
-				echo '<th>';
-					echo "$Main_0 " .$News['News_Date']. " $Main_1 " .$News['News_Account_Pseudo']. "";
-				echo '</th>';
-			echo '</tr>';
-							
-			echo '<tr>';
-				echo '<td>';
-					echo '<h4>' .$News['News_Title']. '</h4>';
-					echo '' .stripslashes(nl2br($News['News_Message'])). '';
-				echo '</td>';
-			echo '</tr>';
-		echo '</table><br /><br />';
-	}
-	$Resultat->closeCursor();	
-}
 ?>
